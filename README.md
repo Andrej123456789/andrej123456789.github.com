add67c96b0cc12262e4c6ea11b7fb1373d57815b
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-12-29 22:00:43 +0100

    Updated data on chart and started using web bundler

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 My perosnal website
 
 Code is public domain
+
+## How to run locally
+1. Install Node.js
+2. `npm install`
+3. `npm run dev`
+
+*Sorry for not plain HTML, SCSS, JS anymore*
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+/* Thanks to Fireship for code */
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+/* ----------------------------------- */
+
+var languages = new Map<string, [number, string]>();
+
+languages.set("VB.NET", [6.66, "#945db7"]);
+languages.set("Python", [6.66, "#3572a5"]);
+languages.set("HTML", [13.33, "#e34c26"]);
+languages.set("Java", [6.66, "#b07219"]);
+languages.set("Rust", [26.66, "#dea584"]);
+languages.set("Assembly", [6.66, "#6e4c13"]);
+languages.set("C++", [6.66, "#f34b7d"]);
+languages.set("C", [13.33, "#555555"]);
+languages.set("Dart", [6.66, "#00b4ab"]);
+
+const mapArray = Array.from(languages);
+mapArray.sort((a, b) => b[1][0] - a[1][0]);
+const sortedMap = new Map(mapArray);
+
+console.log(sortedMap);
+
+const firstStrings = Array.from(sortedMap.entries()).map(([key, value]) => key);
+const numbers = Array.from(sortedMap.entries()).map(([key, value]) => value[0]);
+const secondStrings = Array.from(sortedMap.entries()).map(([key, value]) => value[1]);
+
+
+/*
+ * Thanks for ChatGPT for this part
+ * Get a reference to the canvas element
+*/
+const canvas = document.getElementById("chart");
+
+/*
+ * Thanks for ChatGPT for this part
+ * Create a new pie chart using Chart.js
+*/
+const chart = new Chart(canvas, {
+  type: "pie",
+  data: {
+    labels: firstStrings,
+    datasets: [
+      {
+        data: numbers,
+        backgroundColor: secondStrings
+      },
+    ],
+  },
+  options: {
+    animation: {
+      animateRotate: true,
+      animateScale: true,
+    },
+  },
+});
